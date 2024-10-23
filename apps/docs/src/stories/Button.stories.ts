f83534429559c0234b8daad19b1c2c337d3c9261
Pedro Mello <contato@cicatriz.dev>
2024-10-23 16:29:39 -0300

    c1a5.2

--- apps/docs/src/stories/Button.stories.ts
+++ apps/docs/src/stories/Button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { Button } from '@alfabit/button';
+import { Button } from '@cicatrizdev/alfabit-button';
 
 const meta = {
 	title: 'Example/Button',
