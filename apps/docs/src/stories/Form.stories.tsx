ef1556d361e974816f976135d7d9955fcd929a19
Pedro Mello <contato@cicatriz.dev>
2024-11-02 14:31:26 -0300

    c2a3.3

--- apps/docs/src/stories/Form.stories.tsx
+++ apps/docs/src/stories/Form.stories.tsx
@@ -1,15 +1,59 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Input } from '@cicatrizdev/alfabit-input';
+import { Button } from '@cicatrizdev/alfabit-button';
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+`;
+
+const StyledInputField = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+	margin-bottom: 1rem;
+`;
+
+const StyledInput = styled(Input)`
+	width: 100%;
+`;
+
+const StyledButtonField = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+`;
+
+const Form = () => {
+	return (
+		<StyledForm>
+			<StyledInputField>
+				<StyledInput label='Nome' placeholder='ex: João da Silva' />
+			</StyledInputField>
+			<StyledInputField>
+				<StyledInput label='Email' placeholder='ex: joaodasilva@gmail.com' />
+			</StyledInputField>
+			<StyledButtonField>
+				<Button>Enviar</Button>
+			</StyledButtonField>
+		</StyledForm>
+	);
+};
 
 const meta = {
 	title: 'Organismos/Form',
-	component: () => <div>Hello world</div>,
+	component: Form,
 	parameters: {
 		layout: 'centered',
 	},
 	tags: ['autodocs'],
 	// args: { disabled: false, placeholder: 'Placeholder' },
-} satisfies Meta<typeof Input>;
+} satisfies Meta<typeof Form>;
 
 export default meta;
-// type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
