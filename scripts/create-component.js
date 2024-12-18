e2adb9b21390738be16e8bf0bcf8298377b5acdd
Pedro Mello <contato@cicatriz.dev>
2024-12-18 17:56:58 -0300

    c3a2.2

--- scripts/create-component.js
+++ scripts/create-component.js
@@ -2,8 +2,34 @@ const fs = require('fs-extra');
 const path = require('path');
 
 async function createComponent(componentName) {
-  try {
-    const templateDir = path.join(__dirname, '../packages/component-template');
-    const targetDir = path.join(__dirname, '../packages', `alfabit-${componentName.toLowerCase()}`);
-  }
+	try {
+		const templateDir = path.join(__dirname, '../packages/component-template');
+		const targetDir = path.join(__dirname, '../packages', `alfabit-${componentName.toLowerCase()}`);
+
+		if (!fs.existsSync(templateDir)) {
+			throw new Error(`Não foi encontrado o diretório de template ${targetDir}`);
+		}
+
+		if (fs.existsSync(targetDir)) {
+			throw new Error(`O diretório do componente ${targetDir} já existe`);
+		}
+
+		await fs.copy(templateDir, targetDir);
+		console.log(`Template copiado com sucesso para ${targetDir}`);
+
+		const files = await fs.readdir(targetDir);
+
+		for (const file of files) {
+			const filePath = path.join(targetDir, file);
+			const stat = await fs.stat(filePath);
+
+			if (stat.isDirectory()) {
+				const subFiles = await fs.readdir(filePath);
+				files.push(...subFiles.map((subFile) => path.join(file, subFile)));
+				continue;
+			}
+		}
+	} catch (error) {
+		console.error(`Erro ao criar o componente ${componentName}:`, error);
+	}
 }
