f430e3372969f66b690ac3c7d2a3a85a91b36fff
Pedro Mello <contato@cicatriz.dev>
2025-01-17 14:52:50 -0300

    c4a1.3

--- packages/alfabit-tokens/src/themes.ts
+++ packages/alfabit-tokens/src/themes.ts
@@ -1,3 +1,21 @@
-export const lightTheme = {};
+import { background, blue, gray, green, red, yellow } from './colors';
+
+const commonColors = {
+	black: '#000000',
+	white: '#FFFFFF',
+};
+
+export const lightTheme = {
+	colors: {
+		...commonColors,
+		background: background.light,
+		text: gray.primary,
+		primary: blue.primary,
+		secondary: gray.secondary,
+		success: green.primary,
+		error: red.primary,
+		warning: yellow.primary,
+	},
+};
 
 export const darkTheme = {};
