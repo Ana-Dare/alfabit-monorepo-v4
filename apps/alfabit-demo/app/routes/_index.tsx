61027300b676ecefc63ccec1416937680c9abe1f
Pedro Mello <contato@cicatriz.dev>
2025-01-19 19:09:03 -0300

    c4a3.2

--- apps/alfabit-demo/app/routes/_index.tsx
+++ apps/alfabit-demo/app/routes/_index.tsx
@@ -1,11 +1,14 @@
 import { Button } from '@cicatrizdev/alfabit-button';
+import { darkTheme, lightTheme, Theme } from '@cicatrizdev/alfabit-tokens';
 import type { MetaFunction } from '@remix-run/node';
+import { useState } from 'react';
 
 export const meta: MetaFunction = () => {
 	return [{ title: 'Alfabit Demo' }, { name: 'description', content: '' }];
 };
 
 export default function Index() {
+	const [appTheme, setAppTheme] = useState<Theme | undefined>(undefined);
 	return (
 		<div className='flex h-screen items-center justify-center'>
 			<div className='flex flex-col items-center gap-16'>
@@ -14,7 +17,13 @@ export default function Index() {
 						Bem-vindo ao Design System Alfabit
 					</h1>
 				</header>
-				<Button>Hello World, Alfabit npm!</Button>
+				<Button theme={appTheme}>Hello World, Alfabit npm!</Button>
+				<Button
+					variant='secondary'
+					onClick={() => setAppTheme(appTheme === lightTheme || !appTheme ? darkTheme : lightTheme)}
+				>
+					Trocar o tema
+				</Button>
 			</div>
 		</div>
 	);
