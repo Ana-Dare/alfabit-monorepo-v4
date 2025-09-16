9b56c1ef1c313a0aa6f51671de672aae1783b8ac
Ana-Dare <analauradare467@gmail.com>
2025-09-16 09:37:01 -0300

    feat: create themes

--- packages/alfabit-button/src/Button.tsx
+++ packages/alfabit-button/src/Button.tsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import { StyledButton } from './Button.styles';
-import { defaultTheme, Theme } from '@cicatrizdev/alfabit-tokens';
+import React from "react";
+import { StyledButton } from "./Button.styles";
+import { defaultTheme, ITheme } from "@cicatrizdev/alfabit-tokens";
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	variant?: 'primary' | 'secondary' | 'tertiary';
-	size?: 'xs' | 'sm' | 'md' | 'lg';
-	children: string;
-	icon?: React.ReactNode;
-	iconPosition?: 'left' | 'right';
-	disabled?: boolean;
-	onClick?: () => void;
-	theme?: Theme;
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "tertiary";
+  size?: "xs" | "sm" | "md" | "lg";
+  children: string;
+  icon?: React.ReactNode;
+  iconPosition?: "left" | "right";
+  disabled?: boolean;
+  onClick?: () => void;
+  theme?: ITheme;
 }
 
 export const Button = ({
-	variant = 'primary',
-	size = 'md',
-	children,
-	disabled,
-	onClick,
-	theme = defaultTheme,
+  variant = "primary",
+  size = "md",
+  children,
+  disabled,
+  onClick,
+  theme = defaultTheme,
 }: ButtonProps) => {
-	return (
-		<StyledButton size={size} variant={variant} disabled={disabled} onClick={onClick} theme={theme}>
-			{children}
-		</StyledButton>
-	);
+  return (
+    <StyledButton
+      size={size}
+      variant={variant}
+      disabled={disabled}
+      onClick={onClick}
+      theme={theme}
+    >
+      {children}
+    </StyledButton>
+  );
 };
--- packages/alfabit-tokens/src/themes.ts
+++ packages/alfabit-tokens/src/themes.ts
@@ -1,47 +1,60 @@
-import { background, blue, gray, green, red, yellow } from './colors';
+import { background, blue, gray, green, red, yellow } from "./colors";
 
-export type Theme = {
-	colors: {
-		background: string;
-		text: string;
-		primary: string;
-		secondary: string;
-		success: string;
-		error: string;
-		warning: string;
-		black: string;
-		white: string;
-		disabled: string;
-	};
-};
+export interface ITheme {
+  colors: Icolors;
+}
+
+interface Icolors { 
+  background: string;
+  primary: string;
+  text: string;
+  secondary: string;
+  success: string;
+  error: string;
+  warning: string;
+  black: string;
+  white: string;
+}
 
 const commonColors = {
-	black: gray.black,
-	white: gray.white,
-	success: green.primary,
-	error: red.primary,
-	warning: yellow.primary,
-	disabled: gray.disabled,
+  black: gray.black,
+  white: gray.white,
 };
 
-export const lightTheme: Theme = {
-	colors: {
-		...commonColors,
-		background: background.light,
-		text: gray.primary,
-		primary: blue.primary,
-		secondary: gray.secondary,
-	},
+export const lightTheme: ITheme = {
+  colors: {
+    ...commonColors,
+    background: background.light,
+    primary: blue.primary,
+    text: gray.primary,
+    secondary: gray.secondary,
+    success: green.primary,
+    error: red.primary,
+    warning: yellow.primary,
+  },
 };
 
-export const darkTheme: Theme = {
-	colors: {
-		...commonColors,
-		background: gray.primary,
-		text: gray.white,
-		primary: blue.tertiary,
-		secondary: gray.tertiary,
-	},
+export const darkTheme: ITheme = {
+  colors: {
+    ...commonColors,
+    background: gray.primary,
+    text: gray.white,
+    primary: blue.tertiary,
+    secondary: gray.tertiary,
+    success: green.primary,
+    error: red.primary,
+    warning: yellow.primary,
+  },
 };
 
-export const defaultTheme: Theme = lightTheme;
+export const defaultTheme: ITheme = lightTheme;
+
+// Funçaõ alternar entre temas (desfaio)
+function toggleTheme(currentTheme: ITheme): ITheme {
+  return currentTheme === lightTheme ? darkTheme : lightTheme;
+}
+
+let currentheme = lightTheme;
+console.log("Tema atual", currentheme);
+currentheme = toggleTheme(currentheme);
+console.log("Tema alternado", currentheme);
